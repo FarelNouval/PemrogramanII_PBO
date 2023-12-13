0cf576cf684456935fb925d1a3e43f618b8aab43
Farel Nouval <fareldazz123@gmail.com>
2023-12-13 15:19:06 +0700

    p11

---
+++ PERTEMUAN11/PRAKTEK/P9_1214070/P9_1214070/view/Form1.cs
@@ -0,0 +1,153 @@
+﻿using P9_1214070.controller;
+using P9_1214070.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_1214070
+{
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi = new Koneksi();
+        M_mahasiswa m_mhs = new M_mahasiswa();
+
+        public void Tampil()
+        {
+            //Query DB Get MGS
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+            //Mengubah Nama Kolom Tabel
+            DataMahasiswa.Columns[0].HeaderText = "NPM";
+            DataMahasiswa.Columns[1].HeaderText = "Nama";
+            DataMahasiswa.Columns[2].HeaderText = "Angkatan";
+            DataMahasiswa.Columns[3].HeaderText = "Alamat";
+            DataMahasiswa.Columns[4].HeaderText = "Email";
+            DataMahasiswa.Columns[5].HeaderText = "No HP";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        public void ResetForm()
+        {
+            npm.Text = "";
+            nama.Text = "";
+            angkatan.SelectedIndex = -1;
+            alamat.Text = "";
+            email.Text = "";
+            nohp.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Tampil();
+        }
+
+        private void DataMahasiswa_AutoSizeColumnsModeChanged(object sender, DataGridViewAutoSizeColumnsModeEventArgs e)
+        {
+
+        }
+
+        private void btnsimpan_Click(object sender, EventArgs e)
+        {
+            
+
+            if (npm.Text == "" || nama.Text == "" || angkatan.SelectedIndex == -1 ||
+            alamat.Text == "" || email.Text == "" || nohp.Text == "")
+            {
+                MessageBox.Show("Data tidak boleh kosong", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                m_mhs.Npm = npm.Text;
+                m_mhs.Nama = nama.Text;
+                m_mhs.Angkatan = angkatan.Text;
+                m_mhs.Alamat = alamat.Text;
+                m_mhs.Email = email.Text;
+                m_mhs.Nohp = nohp.Text;
+
+                mhs.Insert(m_mhs);
+
+                ResetForm();
+                Tampil();
+            }
+        }
+
+        private void npm_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataMahasiswa_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            npm.Text = DataMahasiswa.Rows[e.RowIndex].Cells[0].Value.ToString();
+            nama.Text = DataMahasiswa.Rows[e.RowIndex].Cells[1].Value.ToString();
+            angkatan.Text = DataMahasiswa.Rows[e.RowIndex].Cells[2].Value.ToString();
+            alamat.Text = DataMahasiswa.Rows[e.RowIndex].Cells[3].Value.ToString();
+            email.Text = DataMahasiswa.Rows[e.RowIndex].Cells[4].Value.ToString();
+            nohp.Text = DataMahasiswa.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+
+        private void btnubah_Click(object sender, EventArgs e)
+        {
+            if (npm.Text == "" || nama.Text == "" || angkatan.SelectedIndex == -1 ||
+            alamat.Text == "" || email.Text == "" || nohp.Text == "")
+            {
+                MessageBox.Show("Data tidak boleh kosong", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                m_mhs.Npm = npm.Text;
+                m_mhs.Nama = nama.Text;
+                m_mhs.Angkatan = angkatan.Text;
+                m_mhs.Alamat = alamat.Text;
+                m_mhs.Email = email.Text;
+                m_mhs.Nohp = nohp.Text;
+
+                mhs.Update(m_mhs, npm.Text);
+
+                ResetForm();
+                Tampil();
+            }
+        }
+
+        private void btnhapus_Click(object sender, EventArgs e)
+        {
+            DialogResult pesan = MessageBox.Show(
+                "Apakah yakin akan menghapus data ini?",
+                "Perhatian",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if(pesan == DialogResult.Yes)
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                mhs.Delete(npm.Text);
+                ResetForm();
+                Tampil();
+            }
+        }
+
+        private void btnrefresh_Click(object sender, EventArgs e)
+        {
+            Tampil();
+            ResetForm();
+
+            MessageBox.Show("Data berhasil direfresh", "Informasi",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
